6f94639543d720c302bf1932a86a9a783e819894
annde1 <annadepta61@gmail.com>
2024-06-19 17:52:20 +0300

    lectures 316-321

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,1 +1,37 @@
 //This file will create path to send requests to: /api/feedback
+//Here we don't export components
+//Instead we should export a function
+//Any code included here will never be exposed to the client side. This code is only sever side
+
+import fs from "fs";
+import path from "path";
+function handler(req, res) {
+  //We can chain methods
+
+  //We need to figure out which kind of request is triggering this function. By default all the methods (GET,POST, etc...) will trigger this request
+  if (req.method === "POST") {
+    //Extract and store the data in the database (in the case of this project in a file)
+    const { email, feedback } = req.body; //next js will automatically parse the body of the incoming request
+    const newFeedback = {
+      email,
+      feedback,
+      id: new Date().toISOString(),
+    }; //we can now store in the database
+
+    //Construct absolute path to the file
+    const filePath = path.join(process.cwd(), "data", "feedback.json"); //process.cwd() refers to the current working directory (overall project directory)
+    const fileData = fs.readFileSync(filePath); //read the existing data store in the file
+    const data = JSON.parse(fileData);
+    data.push(newFeedback); //push the incoming data to the data's array
+    fs.writeFileSync(filePath, JSON.stringify(data)); // stringify the data and write the file back to the disk
+
+    //Send back a response:
+    res.status(201).json({
+      message: "Feedbacl stored in the database.",
+      feedback: newFeedback,
+    });
+  } else {
+    res.status(200).json({ message: "Hello Kitzy" });
+  }
+}
+export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,44 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(e) {
+    e.preventDefault();
+
+    //Extract value and send request
+    const emailValue = emailInputRef.current.value;
+    const feedbackValue = feedbackInputRef.current.value;
+
+    //We could do validation here
+    const reqBody = {
+      email: emailValue,
+      feedback: feedbackValue,
+    };
+    //Configure and send request
+    const response = fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  } //onSubmit on form will bid this handler to that form
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feddback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
